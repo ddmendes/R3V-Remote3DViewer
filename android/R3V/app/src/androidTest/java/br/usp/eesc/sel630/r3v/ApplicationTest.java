dd97d6fd5327a25b6671f991288255c0f09b54f1
Davi Diorio Mendes <ddioriomendes@gmail.com>
2015-08-18 14:46:20 -0300

    Android project added.

---
+++ android/R3V/app/src/androidTest/java/br/usp/eesc/sel630/r3v/ApplicationTest.java
@@ -0,0 +1,13 @@
+package br.usp.eesc.sel630.r3v;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
