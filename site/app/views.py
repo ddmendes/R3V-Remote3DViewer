be2b0e382ac830f63540f91f2cf9907f2142be60
Henrique Alberto Rusa <henrique.rusa@gmail.com>
2015-09-23 16:01:35 -0300

    Adding templates to site

--- site/app/views.py
+++ site/app/views.py
@@ -4,7 +4,6 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     # user = {'nickname': 'Miguel'}  # fake user
     # posts = [  # fake array of posts
@@ -18,3 +17,36 @@ def index():
     #     }
     # ]
     return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    # user = {'nickname': 'Miguel'}  # fake user
+    # posts = [  # fake array of posts
+    #     {
+    #         'author': {'nickname': 'John'},
+    #         'body': 'Beautiful day in Portland!'
+    #     },
+    #     {
+    #         'author': {'nickname': 'Susan'},
+    #         'body': 'The Avengers movie was so cool!'
+    #     }
+    # ]
+    return render_template("about.html")
+
+@app.route('/camerastream')
+def camera_stream():
+    # user = {'nickname': 'Miguel'}  # fake user
+    # posts = [  # fake array of posts
+    #     {
+    #         'author': {'nickname': 'John'},
+    #         'body': 'Beautiful day in Portland!'
+    #     },
+    #     {
+    #         'author': {'nickname': 'Susan'},
+    #         'body': 'The Avengers movie was so cool!'
+    #     }
+    # ]
+    return render_template("camerastream.html")
+
+
