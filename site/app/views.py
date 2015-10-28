158710835661083fbccee691151945a4b0f42a00
Davi Diorio Mendes <ddioriomendes@gmail.com>
2015-10-28 15:42:53 -0200

    Successfully streaming video.

--- site/app/views.py
+++ site/app/views.py
@@ -1,14 +1,18 @@
-import requests
-from flask import render_template, request
+﻿from flask import render_template, request, Response
 from app import app
+import requests
+import urllib
 import pdb
 
 
 ipcamProtocol = 'http://'
 ipcamAddress = '143.107.235.49:8085'
 ipcamWebctl = '/decoder_control.cgi'
+ipcamVideostream = '/videostream.cgi'
 ipcamUser = 'sel630'
 ipcamPasswd = 'sel630'
+ipcamResolution = '32'
+ipcamRate = '0'
 ipcamCommands = {'up': 2, 'down': 0, 'left': 6, 'right': 4}
 
 
@@ -18,8 +22,8 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/cam/', methods=['GET', 'POST'])
-def cam():
+@app.route('/camposition/', methods=['GET', 'POST'])
+def camposition():
     # pdb.set_trace()
     cmd = request.args['move']
     degree = request.args['degree']
@@ -32,6 +36,48 @@ def cam():
                       'onestep': 1,
                       'degree': degree})
     return r.text
+
+
+@app.route('/camstream/')
+def camstream():
+    url = "%(protocol)s%(address)s%(cgi)s?user=%(user)s&pwd=%(pwd)s&resolution\
+=%(resolution)s&rate=%(rate)s" % {'protocol': ipcamProtocol,
+                                  'address': ipcamAddress,
+                                  'cgi': ipcamVideostream,
+                                  'user': ipcamUser,
+                                  'pwd': ipcamPasswd,
+                                  'resolution': ipcamResolution,
+                                  'rate': ipcamRate}
+    print url
+    ws = urllib.urlopen(url)
+
+    def stream():
+        while(True):
+            res = ""
+
+            s = ws.readline()
+            res = res + s
+
+            s = ws.readline()
+            res = res + s
+
+            s = ws.readline()
+            res = res + s
+
+            length = int(s.split(':')[1].strip())
+
+            s = ws.readline()
+            res = res + s
+
+            s = ws.read(length)
+            res = res + s
+
+            s = ws.readline()
+            res = res + s
+
+            yield res
+
+    return Response(stream(), mimetype="multipart/x-mixed-replace; boundary=ipcamera")
 
 
 @app.route('/about/')
